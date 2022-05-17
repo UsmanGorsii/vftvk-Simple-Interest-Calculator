0d69e06217674ca40e0a857b004ecd0c7dea86a7
UsmanGorsii <uch441@gmail.com>
2022-05-17 22:21:51 +0000

    few things left

--- script.js
+++ script.js
@@ -3,18 +3,14 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
-    var interest = principal * year * rate /100;
+    var interest = principal * years * rate /100;
     var year = new Date().getFullYear() + parseInt(years);
 
-    document.getElementById('result').innerText(
-        "If you deposit" + principal +
-        "\<br\>at an interest rate of" + rate + "%\<br\>" +
-        "You will recieve an amount of "+ interest +
-        "\<br\>in the year " + year
-    )
+    document.getElementById('result').innerText ="If you deposit " + principal.toString().bold() + "\nat an interest rate of " + rate.toString().bold() + "%\n" + "You will recieve an amount of "+ interest.toString().bold() + "\nin the year " + year.toString().bold();
+    
 }
 function pricipalCheck(){
-    if(principal<= 0){
+    if(document.getElementById('principal').value <= 0){
         alert("Enter a positive number");
         document.getElementById('principal').focus;
         return false;
@@ -24,6 +20,6 @@ function pricipalCheck(){
 function updateRate(){
     var rate_slider = document.getElementById('rate').value;
     var rate_span = document.getElementById('rate_val');
-    rate_span.innerText = rate_slider;
+    rate_span.innerText = rate_slider + "%";
 }
         
